d686ed53f2fe133cb1f6308f590df408798febc4
VanesStalkers <ivan.mironov@bk.ru>
2025-08-26 01:07:11 +0400

    updated configs/games

--- application/domain/game/configs/games.js
+++ application/domain/game/configs/games.js
@@ -1,37 +1,115 @@
 () => ({
-  TO_CHANGE: {
-    ...{ title: 'TO_CHANGE', icon: ['fas', 'user'] },
+  sales: {
+    ...{ title: 'Банк-продаж', icon: ['fas', 'cash-register'] },
     items: {
-      blitz: {
-        title: 'Блиц',
-        timer: 60,
+      default: {
+        title: 'Дуэль',
       },
-      standart: {
-        title: 'Стандарт',
-        timer: 45,
+      ai: {
+        title: 'Один игрок',
+        difficulty: [
+          { code: 'weak', title: 'Слабый' },
+          { code: 'strong', title: 'Сильный' },
+        ],
       },
-      hardcore: {
-        title: 'Хардкор',
-        timer: 30,
-      },
+      // добавить два уровня сложности: с подсказками (расчет текущей суммы сделки) и без них
     },
     itemsDefault: {
-      singlePlayer: true,
-      timer: 60,
+      timer: (baseTimer) => {
+        return {
+          DEFAULT: baseTimer,
+          SECOND_OFFER: Math.ceil(baseTimer / 2),
+          PRESENT: Math.ceil(baseTimer / 3),
+          SHOW_RESULTS: Math.max(5, Math.ceil(baseTimer / 5)),
+          CARD_DROP: Math.max(5, Math.ceil(baseTimer / 5)),
+        };
+      },
+      winMoneySum: 2000,
       cardsToRemove: [],
+      playerHand: {
+        product: {
+          start: 5,
+          limit: 7,
+        },
+        service: {
+          start: 5,
+          limit: 7,
+        },
+      },
       autoFinishAfterRoundsOverdue: 10,
 
       playerList: [
         {
           _code: 1,
           active: true,
-          deckList: [{ type: 'card', itemType: 'event' }],
+          deckList: [
+            { type: 'card', subtype: 'product' },
+            { type: 'card', subtype: 'service' },
+            { type: 'card', subtype: 'played', placement: 'table' },
+          ],
+        },
+        {
+          _code: 2,
+          active: true,
+          deckList: [
+            { type: 'card', subtype: 'product' },
+            { type: 'card', subtype: 'service' },
+            { type: 'card', subtype: 'played', placement: 'table' },
+          ],
         },
       ],
       deckList: [
-        { type: 'card', itemType: 'event' },
-        { type: 'card', subtype: 'active', itemType: 'event', access: 'all' },
-        { type: 'card', subtype: 'drop', itemType: 'event' },
+        { type: 'card', subtype: 'product', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'service', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'client', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'feature', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'drop', placement: 'drop' },
+        { type: 'card', subtype: 'zone_client', placement: 'table', access: 'all' },
+        { type: 'card', subtype: 'zone_feature', placement: 'table' },
+      ],
+    },
+  },
+  poker: {
+    ...{ title: 'TO_CHANGE', icon: ['fas', 'fa-coins'] },
+    items: {
+      default: {
+        title: 'Стандарт',
+        maxPlayersInGame: '2-8',
+        minPlayersToStart: 2,
+      },
+    },
+    itemsDefault: {
+      timer: (baseTimer) => {
+        return {
+          DEFAULT: baseTimer,
+          SHOW_RESULTS: Math.max(15, Math.ceil(baseTimer / 2)),
+        };
+      },
+      cardsToRemove: [],
+      playerHand: {},
+      autoFinishAfterRoundsOverdue: 10,
+      playerStartMoney: 10000,
+      bigBlindSum: 100,
+
+      playerTemplates: {
+        default: {
+          deckList: [
+            { type: 'card', subtype: 'hand' },
+            { type: 'card', subtype: 'played', placement: 'table', access: 'all' },
+          ],
+        },
+      },
+
+      playerList: [],
+      deckList: [
+        { type: 'card', subtype: 'car', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'service', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'client', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'credit', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'feature', placement: 'main', hasDrop: true },
+        { type: 'card', subtype: 'zone_client', placement: 'table' },
+        { type: 'card', subtype: 'zone_feature', placement: 'table' },
+        { type: 'card', subtype: 'zone_credit', placement: 'table' },
       ],
     },
   },
